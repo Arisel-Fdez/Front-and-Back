9b3fdc83b37f6ee3f23b4ec7379e31f284a63d48
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2022-06-08 09:40:24 -0500

    add orm sequelize

---
+++ config.js
@@ -0,0 +1,10 @@
+export const api = {
+    port: process.env.API_PORT || 3000,
+};
+export const db = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'CSDB',
+    password: 'yeremi224',
+    port: '5432'
+};
