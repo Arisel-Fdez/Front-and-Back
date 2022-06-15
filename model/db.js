2f559c1b45d5d4a257ef72fef8f136890c722326
Arisel-Fdez <211119@ids.upchiapas.edu.mx>
2022-06-15 00:20:21 -0500

    db

--- model/db.js
+++ model/db.js
@@ -1,39 +1,25 @@
-
-import pkg from 'pg';
-import { Sequelize } from 'sequelize';
-const {Pool} = pkg;
-import { db } from '../config.js';
+import Sequelize from 'sequelize';
+import { db } from '../config/config.js';
 
 
-async function getConnection(){
-    const client = new Pool({
-        user: db.user,
-        host: db.host,
-        database: db.database,
-        password: db.password,
-        port: db.port
-    });
-    await client.connect();
-    return client;
-}
 
-const sequelizeClient = new Sequelize(db.database, db.user, db.password,{
+const sequelizeClient = new Sequelize(db.database, db.user, db.password, {
     dialectOptions: {
-        ssl:{
-            require:true,
-            rejectUnauthorized:false
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
     },
     host: db.host,
     dialect: 'postgres',
 });
 
-sequelizeClient.authenticate()
-     .then(() => {
-         console.log('conectado')
-     })
-     .catch(() =>{
-         console.log('No conecto!')
-     });
+sequelizeClient.sync({ force: true })
+    .then(() => {
+        console.log('Conectado')
+    })
+    .catch((err) => {
+        console.log('No se conecto', err)
+    });
 
-export const getData = {getConnection, sequelizeClient};
+export const getData = { sequelizeClient };
